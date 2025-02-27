b0693f9339ef21c3e76cb170c1db794a1a64578e
Bill Garrett <garrett@astralfrontier.org>
2025-02-27 01:41:08 -0800

    Scaffold a new PDF building project

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A tabletop roleplaying supplement published on itch.io";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        projectname = "familiar-faces";
+      in
+      {
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          name = "${projectname}-pdf";
+          nativeBuildInputs = [
+            pkgs.texliveConTeXt
+          ];
+          src = self;
+          buildPhase = ''
+            pushd src/
+            context ${projectname}.tex --purgeall
+          '';
+          installPhase = ''
+            mkdir $out
+            cp ${projectname}.pdf $out/
+          '';
+        };
+        devShell = pkgs.mkShell {
+          name = projectname;
+          packages = [
+            pkgs.texliveConTeXt
+          ];
+        };
+      }
+    );
+}
